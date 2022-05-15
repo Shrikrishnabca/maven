d7616a1dc61ab8c3e2aba8336700466b0f85b15b
krishna <HP@DESKTOP-FUH0V7A>
2022-05-15 15:39:49 +0530

    second commit

---
+++ actiTime/src/main/java/com/ActiTime/generic/BaseClass.java
@@ -0,0 +1,25 @@
+package com.ActiTime.generic;
+
+
+public class BaseClass {
+
+	public void openbrowser()
+	{
+		
+	}
+	public void closebrowser()
+	{
+		
+	}
+	public void login()
+	{
+		
+	}
+	public void logout()
+	{
+		
+	}
+
+
+
+}
