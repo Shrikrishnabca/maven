470efc1bb5f188a9c44a38dc60993145785c3044
krishn <HP@DESKTOP-FUH0V7A>
2022-05-16 22:28:03 +0530

    commit 3

--- actiTime/src/main/java/com/ActiTime/generic/BaseClass.java
+++ actiTime/src/main/java/com/ActiTime/generic/BaseClass.java
@@ -1,23 +1,46 @@
 package com.ActiTime.generic;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 public class BaseClass {
-
-	public void openbrowser()
+	static
 	{
-		
+		System.setProperty("webdriver.chromedriver.driver", "./chromedriver.exe");
 	}
-	public void closebrowser()
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void openBrowser()
 	{
-		
+	Reporter.log("open Browser",true);
+	driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
+	@AfterTest
+	public void closeBrowser()
+	{
+		Reporter.log("close Browser",true);
+		driver.close();
+	}
+	@BeforeMethod
 	public void login()
 	{
-		
+		Reporter.log("Login",true);
 	}
+	@AfterMethod
 	public void logout()
 	{
-		
+		Reporter.log("Logout",true);
 	}
 
 
